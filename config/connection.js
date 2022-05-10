73c16b9076fa34947eaab708961931bdd1741162
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-05-10 09:36:03 +0100

    User model created

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const { connect, connection } = require("mongoose");
 
 // Create a new Mongo Database called socialDB
-connect("mongodb://localhost/socialDB", {
+connect("mongodb://localhost:27017/socialDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require("mongoose");
+// Package used to validate email address
+const validator = require("validator");
+
+// User model schema
+const userSchema = new Schema(
+  {
+    username: { type: String, unique: true, required: true, trim: true },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      validate: [validator.isEmail, "Please use a valid email address"],
+    },
+    age: Number,
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thoughts",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Users",
+      },
+    ],
+  },
+  {
+    // Virtuals enables for friend count
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// Virtual for friend count
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// User model init
+const User = model("user", userSchema);
+
+module.exports = User;
